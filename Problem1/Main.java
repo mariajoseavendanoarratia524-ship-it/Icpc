5c9394596b29c79230aa376b94faed8a9dbc730f
¨Majo¨ <mariajoseavendanoarratia524@gmail.com>
2026-09-19 22:32:54 -0400

    Problem 1

---
+++ Problem1/Main.java
@@ -0,0 +1,15 @@
+package Problem1;
+import java.util.Scanner;
+public class Main{
+public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    int w = sc.nextInt();
+    if (w<=100 && w > 2 && w%2==0){
+        System.out.println("YES");
+    }else {
+        System.out.println("NO");
+    }
+    sc.close();
+    }
+}
+
