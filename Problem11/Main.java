bdd2794db7918ab2c9d62ca1b31beeb737f3625f
¨Majo¨ <mariajoseavendanoarratia524@gmail.com>
2026-09-19 23:41:06 -0400

    Problem11

---
+++ Problem11/Main.java
@@ -0,0 +1,18 @@
+package Problem11;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+
+        int a = lector.nextInt();
+        int b = lector.nextInt();
+        int c = lector.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        lector.close();
+    }
+}
